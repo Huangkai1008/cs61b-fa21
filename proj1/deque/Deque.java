ffeb88a6367ac01fc25fa9377ff164a72fbdabb8
huangkai <h1770360848@outlook.com>
2025-12-17 13:59:54 +0800

    perf(lab): lab3 resize

---
+++ proj1/deque/Deque.java
@@ -0,0 +1,52 @@
+package deque;
+
+/**
+ * Deque (usually pronounced like “deck”) is an irregular acronym of double-ended queue.
+ * Double-ended queues are sequence containers with dynamic sizes that can be expanded or
+ * contracted on both ends (either its front or its back).
+ */
+public interface Deque<T> {
+    /**
+     * Adds an item of type T to the front of the deque. You can assume that item is never null.
+     */
+    public void addFirst(T item);
+
+    /**
+     * Adds an item of type T to the back of the deque. You can assume that item is never null.
+     */
+    public void addLast(T item);
+
+    /**
+     * Returns true if deque is empty, false otherwise.
+     */
+    public boolean isEmpty();
+
+    /**
+     * Returns the number of items in the deque.
+     */
+    public int size();
+
+    /**
+     * Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line.
+     */
+    public void printDeque();
+
+    /**
+     * Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
+     */
+    public T removeFirst();
+
+    /**
+     * Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.
+     */
+    public T removeLast();
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque!
+     */
+    public T get(int index);
+}

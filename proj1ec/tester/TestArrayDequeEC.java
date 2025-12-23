eaa948fd615abb568328394236487e47b96a3a60
huangkai <h1770360848@outlook.com>
2025-12-23 14:37:55 +0800

    refactor(tester): simplify TestArrayDequeEC by removing setup and teardown methods, and introduce a randomized testing method for comprehensive coverage

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,8 +1,6 @@
 package tester;
 
 import edu.princeton.cs.introcs.StdRandom;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import student.StudentArrayDeque;
 
@@ -12,95 +10,49 @@ import static org.junit.Assert.*;
  * @author huangkai
  */
 public class TestArrayDequeEC {
-    private static final int QTY = 100;
-    private ArrayDequeSolution<Integer> deque1;
-    private StudentArrayDeque<Integer> deque2;
-
-    @Before
-    public void setUp() throws Exception {
-        deque1 = new ArrayDequeSolution<>();
-        deque2 = new StudentArrayDeque<>();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        deque1 = null;
-        deque2 = null;
-    }
-
+    
     @Test
-    public void addFirstTest() {
-        for (int i = 0; i <= QTY; i++) {
-            Integer number = StdRandom.uniform(QTY);
-            deque1.addFirst(number);
-            deque2.addFirst(number);
-            assertEquals(deque1.get(i), deque2.get(i));
+    public void randomizedTest() {
+        ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        
+        StringBuilder message = new StringBuilder();
+        
+        for (int i = 0; i < 1000; i++) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            
+            if (operationNumber == 0) {
+                // addFirst
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addFirst(randVal);
+                student.addFirst(randVal);
+                message.append("addFirst(").append(randVal).append(")\n");
+                
+            } else if (operationNumber == 1) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                student.addLast(randVal);
+                message.append("addLast(").append(randVal).append(")\n");
+                
+            } else if (operationNumber == 2) {
+                // removeFirst
+                if (correct.size() > 0) {
+                    message.append("removeFirst()\n");
+                    Integer expected = correct.removeFirst();
+                    Integer actual = student.removeFirst();
+                    assertEquals(message.toString(), expected, actual);
+                }
+                
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (correct.size() > 0) {
+                    message.append("removeLast()\n");
+                    Integer expected = correct.removeLast();
+                    Integer actual = student.removeLast();
+                    assertEquals(message.toString(), expected, actual);
+                }
+            }
         }
-    }
-
-    @Test
-    public void addLastTest() {
-        for (int i = 0; i <= QTY; i++) {
-            Integer number = StdRandom.uniform(QTY);
-            deque1.addLast(number);
-            deque2.addLast(number);
-            assertEquals(deque1.get(i), deque2.get(i));
-        }
-    }
-
-    @Test
-    public void isEmptyTest() {
-        assertTrue(deque1.isEmpty());
-        assertTrue(deque2.isEmpty());
-        for (int i = 0; i <= QTY; i++) {
-            Integer number = StdRandom.uniform(QTY);
-            deque1.addFirst(number);
-            deque2.addFirst(number);
-        }
-        assertFalse(deque1.isEmpty());
-        assertFalse(deque2.isEmpty());
-    }
-
-    @Test
-    public void sizeTest() {
-        assertEquals(deque1.size(), deque2.size());
-        for (int i = 0; i <= QTY; i++) {
-            Integer number = StdRandom.uniform(QTY);
-            deque1.addFirst(number);
-            deque2.addFirst(number);
-        }
-        assertEquals(deque1.size(), deque2.size());
-    }
-
-    @Test
-    public void removeFirstTest() {
-        for (int i = 1; i <= QTY; i++) {
-            Integer number = StdRandom.uniform(QTY);
-            deque1.addFirst(number);
-            deque2.addFirst(number);
-        }
-        for (int i = 1; i <= QTY; i++) {
-            Integer expected = deque1.removeFirst();
-            Integer actual = deque2.removeFirst();
-            assertEquals("Oh no!\nThis is bad:\n   Random number " + " not equal to " + expected + "!", expected, actual);
-            assertEquals(deque1.size(), deque2.size());
-        }
-        assertEquals(deque1.size(), deque2.size());
-    }
-
-    @Test
-    public void removeLastTest() {
-        for (int i = 1; i <= QTY; i++) {
-            Integer number = StdRandom.uniform(QTY);
-            deque1.addFirst(number);
-            deque2.addFirst(number);
-        }
-        for (int i = 1; i <= 10; i++) {
-            Integer expected = deque1.removeLast();
-            Integer actual = deque2.removeLast();
-            assertEquals("Oh no!\nThis is bad:\n   Random number " + " not equal to " + expected + "!", expected, actual);
-            assertEquals(deque1.size(), deque2.size());
-        }
-        assertEquals(deque1.size(), deque2.size());
     }
 }
